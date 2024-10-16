874acb64c63225a61f5172f24ab291b7a94f08be
joasasso <joasasso@gmail.com>
2024-10-16 13:57:55 -0300

    Commit final tateti

--- Practico  Unidad 3/TaTeTi/tateti.js
+++ Practico  Unidad 3/TaTeTi/tateti.js
@@ -1,11 +1,20 @@
-var tablero = [];
-var turno = "O";
-var victoria = "N";
-var cantidadCasillas = 3;
-
-function crearTablero() {
+let tablero = [];
+let turno = "O";
+let victoria = "N";
+let cantidadCasillas = 3;
+let posX;
+let posY;
+let timeoutId;
+let modo;
+let jugadas;
+function crearTablero(tipoPartida) {
 	victoria = "N";
 	turno = "O";
+   modo = tipoPartida;
+   jugadas = 0;
+   posX = 0
+   posY = 0;
+	document.getElementById("turno").innerHTML = "Turno de: O";
 	cantidadCasillas = document.getElementById("cantCasillas").value;
 	var docTablero = document.getElementById("tablero");
 	console.log(cantidadCasillas);
@@ -36,31 +45,51 @@ function crearTablero() {
 }
 
 function toque(columna, fila) {
-   if (victoria == "N") {
+	if (victoria == "N") {
       if (tablero[fila][columna] == 0) {
-         casilla = document.getElementById("c" + columna + fila);
-         if (turno == "O") {
-            casilla.innerHTML = "O";
-            tablero[fila][columna] = turno;
-            turno = "X";
-         } else {
-            casilla.innerHTML = "X";
-            tablero[fila][columna] = turno;
-            turno = "O";
-         }
-         document.getElementById("turno").innerHTML = "Turno de :" + turno;
-         hayGanador();
-      }
+         jugadas++;
+			casilla = document.getElementById("c" + columna + fila);
+			if (turno == "O") {
+				casilla.innerHTML = "O";
+				tablero[fila][columna] = turno;
+				turno = "X";
+			} else {
+				casilla.innerHTML = "X";
+				tablero[fila][columna] = turno;
+				turno = "O";
+			}
+			document.getElementById("turno").innerHTML = "Turno de: " + turno;
+			hayGanador();
+			if (modo == 1 && turno == "X") {
+				jugadaMaquina(fila, columna);
+			}
+		}
 	}
+}
+
+function jugadaMaquina(fila, columna) {
+	let x = columna;
+	let y = fila;
+	while (tablero[x][y] != 0) {
+		x = Math.floor(Math.random() * cantidadCasillas);
+		y = Math.floor(Math.random() * cantidadCasillas);
+	}
+	setTimeout(() => {
+		toque(y,x);
+	}, 1000);
 }
 
 function hayGanador() {
    verificarFilas();
    verificarColumnas();
    verificarDiagonales();
-   if (victoria != "N")
-   {
-      document.getElementById("turno").innerHTML = "EL jugador " + victoria + " ha ganado la partida"
+   if (victoria == "X" || victoria == "O") {
+      document.getElementById("turno").innerHTML =
+         "El jugador " + victoria + " ha ganado la partida";
+   }
+   else if (cantidadCasillas * cantidadCasillas == jugadas) {
+      document.getElementById("turno").innerHTML = "Empate";
+      victoria = "E";
    }
 }
 
@@ -85,55 +114,90 @@ function verificarFilas() {
 	}
 }
 
-function verificarColumnas()
-{
-   for (var i = 0; i < cantidadCasillas; i++) {
-      let contador = 0;
-      for (var j = 0; j < cantidadCasillas; j++) {
-         casilla = tablero[j][i];
-         if (casilla == "O") {
-            contador++;
-         }
-         if (casilla == "X") {
-            contador--;
-         }
-      }
-      if (contador == cantidadCasillas) {
-         victoria = "O";
-      }
-      if (contador == -cantidadCasillas) {
-         victoria = "X";
-      }
-   }
+function verificarColumnas() {
+	for (var i = 0; i < cantidadCasillas; i++) {
+		let contador = 0;
+		for (var j = 0; j < cantidadCasillas; j++) {
+			casilla = tablero[j][i];
+			if (casilla == "O") {
+				contador++;
+			}
+			if (casilla == "X") {
+				contador--;
+			}
+		}
+		if (contador == cantidadCasillas) {
+			victoria = "O";
+		}
+		if (contador == -cantidadCasillas) {
+			victoria = "X";
+		}
+	}
 }
 
-function verificarDiagonales()
-{
-   let contador = 0;
-   let contador2 = 0;
-   for (var i = 0; i < cantidadCasillas; i++) {
-      diagonal = tablero[i][i];
-      antiDiagonal = tablero[i][cantidadCasillas - i - 1];
-      if (diagonal == "O") {
-         contador++;
-      }
-      if (diagonal == "X") {
-         contador--;
-      }
-      if (antiDiagonal == "O")
-      {
-         contador2++;
-      }
-      if (antiDiagonal == "X")
-      {
-         contador2--;   
-      }
-   }
+function verificarDiagonales() {
+	let contador = 0;
+	let contador2 = 0;
+	for (var i = 0; i < cantidadCasillas; i++) {
+		diagonal = tablero[i][i];
+		antiDiagonal = tablero[i][cantidadCasillas - i - 1];
+		if (diagonal == "O") {
+			contador++;
+		}
+		if (diagonal == "X") {
+			contador--;
+		}
+		if (antiDiagonal == "O") {
+			contador2++;
+		}
+		if (antiDiagonal == "X") {
+			contador2--;
+		}
+	}
 
-   if (contador == cantidadCasillas || contador2 == cantidadCasillas) {
-      victoria = "O";
-   }
-   if (contador == -cantidadCasillas || contador2 == -cantidadCasillas) {
-      victoria = "X";
-   }
+	if (contador == cantidadCasillas || contador2 == cantidadCasillas) {
+		victoria = "O";
+	}
+	if (contador == -cantidadCasillas || contador2 == -cantidadCasillas) {
+		victoria = "X";
+	}
 }
+
+document.addEventListener("keydown", (event) => {
+	casilla = document.getElementById("c" + posX + posY);
+	casilla.style.backgroundColor = "white";
+	if (event.key === "ArrowUp") {
+		if (posY > 0) {
+			posY--;
+		}
+	} else if (event.key === "ArrowDown") {
+		if (posY < cantidadCasillas - 1) {
+			posY++;
+		}
+	} else if (event.key === "ArrowLeft") {
+		if (posX > 0) {
+			posX--;
+		}
+	} else if (event.key === "ArrowRight") {
+		if (posX < cantidadCasillas - 1) {
+			posX++;
+		}
+	} else if (event.key === "Enter") {
+		toque(posX, posY);
+	} else if (event.key === "r") {
+		crearTablero(modo);
+	}
+	console.log("posX: " + posX + " posY: " + posY);
+	casilla = document.getElementById("c" + posX + posY);
+	casilla.style.backgroundColor = "aquamarine";
+
+	if (timeoutId) {
+		clearTimeout(timeoutId);
+	}
+	timeoutId = setTimeout(() => {
+		let casillas = document.getElementsByClassName("casilla");
+		for (let i = 0; i < casillas.length; i++) {
+			casillas[i].style.backgroundColor = "white";
+		}
+	}, 3000);
+});
